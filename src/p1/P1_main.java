e1da657c502d0e13e74dd3107a1df22124e2723d
Lara Geschonneck <larageschonneck@MacBook-Pro-von-Lara.local>
2025-10-28 10:39:27 +0100

    Bearbeitung p2

--- src/p1/P1_main.java
+++ src/p1/P1_main.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 
 public class P1_main {
 	public static void main(String[] args) {
-	
-	Scanner s = new Scanner(System.in);
-	String zeile = new String();
-	zeile = s.nextLine();
-	System.out.println(zeile);
-	s.close();
+
+		Scanner s = new Scanner(System.in);
+		String zeile = new String();
+		zeile = s.nextLine();
+		System.out.println(zeile);
+		s.close();
 	}
 }
---
+++ src/p2/P2_main.java
@@ -0,0 +1,12 @@
+package p2;
+
+public class P2_main {
+		public static void main(String[] args) {
+			int i = 18, b = 1;
+			boolean t;
+		    t = (i > b);
+		    System.out.println(t) ;
+		}
+
+
+}
